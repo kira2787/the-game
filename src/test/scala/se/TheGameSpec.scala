c4b17370b22b4d671ba6646ca154d5e8c2ff5b72
kira2787 <=>
2020-11-27 09:03:13 +0100

    Error handling in TheGameSpec

--- src/test/scala/se/TheGameSpec.scala
+++ src/test/scala/se/TheGameSpec.scala
@@ -4,8 +4,9 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class TheGameSpec extends AnyWordSpec with Matchers {
-  "The Game main class should"
+  "The Game main class " should {
     "accept text input as argument" in {
       TheGame.main(Array[String]("s"))
     }
+  }
 }
